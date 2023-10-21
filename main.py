6bd2382eeea38074f97ec95d189de1c785dc84a9
Robert Horvat <robert.horvath93@gmail.com>
2023-10-21 23:49:51 +0300

    created window

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#blank
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()  # we call the parent init method (QMainWindow)
+        self.setWindowTitle("SQL App")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)  # note it's important to connect to the class using self
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+
+app = QApplication(sys.argv)
+age_calculator = MainWindow()
+age_calculator.show()
+sys.exit(app.exec())
